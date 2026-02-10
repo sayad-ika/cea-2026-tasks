2ed52e04d4d9818152063fd68b86a4a92262c836
sayad_ika <sayad.ibn@craftsmensoftware.com>
2026-02-10 13:57:12 +0600

    feat: add profiledropdown and implement logout function

--- 01-task-1-craftsbite/craftsbite-frontend/src/components/layout/index.ts
+++ 01-task-1-craftsbite/craftsbite-frontend/src/components/layout/index.ts
@@ -2,10 +2,13 @@ export { Header } from './Header';
 export type { HeaderProps } from './Header';
 
 export { Navbar } from './Navbar';
-export type { NavbarProps, NavItem } from './Navbar';
+export type { NavItem } from './Navbar';
 
 export { Footer } from './Footer';
 export type { FooterProps, FooterLink } from './Footer';
 
 export { BottomActionButtons } from './BottomActionButtons';
 export type { BottomActionButtonsProps, ActionButton } from './BottomActionButtons';
+
+export { ProfileDropdown } from './ProfileDropdown';
+export type { ProfileDropdownProps } from './ProfileDropdown';
