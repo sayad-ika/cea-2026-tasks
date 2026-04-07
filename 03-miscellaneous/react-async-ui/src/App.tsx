349c649e746bafeddd0960e33979a5846bb0a0f3
sayad_ika <sayad.ibn@craftsmensoftware.com>
2026-04-07 14:54:58 +0600

    chore: initial react project setup with tailwindcss & shadcn/ui"

---
+++ 03-miscellaneous/react-async-ui/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="w-full m-auto p-4">
+                <h1 className="flex justify-center items-center h-full text-4xl font-bold">
+                    React Async UI
+                </h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
